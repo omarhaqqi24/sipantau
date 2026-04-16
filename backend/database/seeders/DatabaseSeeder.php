be8b4618c4a9f1741200eb90dd802d392adb42e3
omarhaqqi24 <muhammadomarhaqqi@gmail.com>
2026-04-16 20:57:03 +0700

    Fixed Database Seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Komoditas;
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,21 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        $this->call(
-                // UserSeeder::class
-
-            RolePermissionSeeder::class
-        );
-
-        $this->call(
+        $this->call([
+            RolePermissionSeeder::class,
             KomoditasSeeder::class
-        );
+        ]);
     }
 }
